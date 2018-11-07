2bfb423a085513b13d1f58386ce2955bdc7f6739
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-11-07 12:35:04 -0500

    removed satellite struct fields and sql fields and join

--- gpkg/observation.go
+++ gpkg/observation.go
@@ -9,36 +9,21 @@ import (
 )
 
 type Observation struct {
-	Id                  int32     `json:"id"`
-	SysTime             time.Time `json:"sys_time"`
-	Lat                 float64   `json:"lat"`
-	Lon                 float64   `json:"lon"`
-	Alt                 float64   `json:"alt"`
-	Provider            string    `json:"provider"`
-	GpsTime             int64     `json:"gps_time"`
-	FixSatCount         int8      `json:"fix_sat_count"`
-	HasRadialAccuracy   bool      `json:"has_radial_accuracy"`
-	HasVerticalAccuracy bool      `json:"has_vertical_accuracy"`
-	RadialAccuracy      float64   `json:"radial_accuracy"`
-	VerticalAccuracy    float64   `json:"vertical_accuracy"`
-	Speed               float64   `json:"speed"`
-	SpeedAccuracy       float64   `json:"speed_accuracy"`
-	SatId               int32     `json:"sat_id"`
-	SatLocalTime        int64     `json:"sat_local_time"`
-	SatVehicleId        int8      `json:"sat_vehicle_id"`
-	SatConstellation    string    `json:"sat_constellation"`
-	SatCn0              float64   `json:"sat_cn_0"`
-	SatAgc              float64   `json:"sat_agc"`
-	SatHasAgc           bool      `json:"sat_has_agc"`
-	SatInFix            bool      `json:"sat_in_fix"`
-	SatTimeNanos        float64   `json:"sat_time_nanos"`
-	SatTime1SigmaNanos  float64   `json:"sat_time_1_sigma_nanos"`
-	SatHasCarrier       bool      `json:"sat_has_carrier"`
-	SatPseudoRateMps    float64   `json:"sat_pseudo_rate_mps"`
-	SatPseudoRate1Sigma float64   `json:"sat_pseudo_rate_1_sigma"`
-	SatHasEphemeris     bool      `json:"sat_has_ephemeris"`
-	SatAzimuth          float64   `json:"sat_azimuth"`
-	SatElevation        float64   `json:"sat_elevation"`
+	Id                  int32       `json:"id"`
+	SysTime             time.Time   `json:"sys_time"`
+	Lat                 float64     `json:"lat"`
+	Lon                 float64     `json:"lon"`
+	Alt                 float64     `json:"alt"`
+	Provider            string      `json:"provider"`
+	GpsTime             int64       `json:"gps_time"`
+	FixSatCount         int8        `json:"fix_sat_count"`
+	HasRadialAccuracy   bool        `json:"has_radial_accuracy"`
+	HasVerticalAccuracy bool        `json:"has_vertical_accuracy"`
+	RadialAccuracy      float64     `json:"radial_accuracy"`
+	VerticalAccuracy    float64     `json:"vertical_accuracy"`
+	Speed               float64     `json:"speed"`
+	SpeedAccuracy       float64     `json:"speed_accuracy"`
+	Satellites          []Satellite `json:"satellites"`
 }
 
 func (o *Observation) getInterfacePtrs() []interface{} {
@@ -89,30 +74,11 @@ func (o *Observation) getSQLQuery() string {
 				P.RadialAccuracy,                                                   
 				P.VerticalAccuracy,                                                 
 				P.Speed,                                                            
-				P.SpeedAccuracy,                                                    
-				S.id as sat_id,                                                     
-				S.local_time as sat_local_time,                                     
-				S.svid as sat_svid,                                                 
-				S.constellation as sat_constellation,                               
-				S.cn0 as sat_cn0,                                                   
-				S.agc as sat_agc,                                                   
-				S.has_agc as sat_has_agc,                                           
-				S.in_fix as sat_in_fix,                                             
-				S.sat_time_nanos,                                                   
-				S.sat_time_1sigma_nanos,                                            
-				S.has_carrier_freq as sat_has_carrier_freq,                         
-				S.pseudorange_rate_mps,                                             
-				S.pseudorange_rate_1sigma,                                          
-				S.has_ephemeris as sat_has_ephemeris,                               
-				S.azimuth_deg as sat_azimuth_deg,                                   
-				S.elevation_deg as sat_elevation_deg                               
+				P.SpeedAccuracy                                                    
            	FROM gps_observation_points_sat_data AS G                              
 			LEFT JOIN                                                         
 			gps_observation_points AS P                                       
 			ON G.base_id = P.id                                               
-			LEFT JOIN                                                         
-			sat_data as S                                                     
-			ON G.related_id = S.id                                            
            	ORDER BY G.base_id
 			`
 }
